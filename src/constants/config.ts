6e70d76d97d95543098dd317b84d31d4f2c376fe
Nahee-Park <nahee@ewha.ac.kr>
2025-04-29 04:30:24 +0900

    feat: update og and favicon

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 // Config file for your website
 export const SiteConfig = {
-  url: 'https://nahee-park.github.io',
+  url: 'https://mail-to-rep.vercel.app',
   pathPrefix: '/',
   title: 'SKT 개인정보 유출 사건 국민행동',
   subtitle: '국회의원들에게 항의 메일을 보내는 플랫폼',
   copyright: 'Nahee-Park © All rights reserved.',
-  googleAnalyticsId: 'YOUR_GOOGLE_ANALYTICS_TRACKING_ID',
+  googleAnalyticsId: 'G-N772SEB2NM',
   author: {
     name: 'Nahee-park',
     photo: '/profile.png',
